3c30549e27ac46344d533abcba77c33f3fef697d
Roy Taragan <roy.taragan@eko.com>
2023-05-13 02:26:22 +0300

    More functionality

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const imageUrls = [];
 async function submit() {
     const email = document.getElementById("email").value;
     const subject = document.getElementById("subject").value;
+    const text = `seed=46
+0:sks ${subject} hitting the drums in office, highly detailed face ,cinematic composition, beautiful lighting, sharp, details, hyper - detailed, hdr, 4 k, 8 k, bright light, pale sunrise, cinematic lighting, ultra realistic, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, HDR 4k, 8k
+100:sks ${subject} playing guitar in office, highly detailed face ,cinematic composition, beautiful lighting, sharp, details, hyper - detailed, hdr, 4 k, 8 k, bright light, pale sunrise, cinematic lighting, ultra realistic, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, HDR 4k, 8k
+200:sks ${subject} in a picture with a pregnant woman, in 21st century in 21st century,bright light, pale sunrise, cinematic lighting, ultra realistic,highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k
+300:sks ${subject} in an family picture with 2 babies, stunning living room in 21st century in 21st century, bright light, pale sunrise, cinematic lighting, ultra realistic,, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k
+360:sks ${subject} in a family picture as an old man with his old wife his adult kids and 8 grandchildren , in 21st century in 21st century, bright light, pale sunrise, cinematic lighting, stunning living room, ultra realistic,,highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k`
     let request = {
         tune: {
             name: subject, // man, woman or person
             title: email,
             branch: 'sd21',
-            prompt_attributes: [{
+            prompts_attributes: [{
                 callback: `${CALLBACK_URL}?email=${email}`,
-                text: `seed=46
-                0:sks ${subject} hitting the drums in office, highly detailed face ,cinematic composition, beautiful lighting, sharp, details, hyper - detailed, hdr, 4 k, 8 k, bright light, pale sunrise, cinematic lighting, ultra realistic, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, HDR 4k, 8k
-                100:sks ${subject} playing guitar in office, highly detailed face ,cinematic composition, beautiful lighting, sharp, details, hyper - detailed, hdr, 4 k, 8 k, bright light, pale sunrise, cinematic lighting, ultra realistic, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, HDR 4k, 8k
-                200:sks ${subject} in a picture with a pregnant woman, in 21st century in 21st century,bright light, pale sunrise, cinematic lighting, ultra realistic,highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k
-                300:sks ${subject} in an family picture with 2 babies, stunning living room in 21st century in 21st century, bright light, pale sunrise, cinematic lighting, ultra realistic,, highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k
-                360:sks ${subject} in a family picture as an old man with his old wife his adult kids and 8 grandchildren , in 21st century in 21st century, bright light, pale sunrise, cinematic lighting, stunning living room, ultra realistic,,highly detailed, depth of field, f/1.8, 85mm, medium shot, mid shot, (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, hdr 4k, 8k`
+                text
             }],
             image_urls: imageUrls
         }
@@ -29,6 +30,9 @@ async function submit() {
         mode: "cors",
         body: JSON.stringify(request)
     });
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('thanks').style.display = 'flex';
+
 }
 function updateSubmitState() {
     const submitBtn = document.getElementById('submitBtn');
